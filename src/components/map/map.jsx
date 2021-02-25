c824c367459af5162778008ac713050d651e00a5
Konstantin <2323466@mail.ru>
2021-02-25 22:17:21 +0300

    Правки ДЗ модуль4-таск1

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -14,41 +14,43 @@ const Map = ({points}) => {
   };
 
   useEffect(() => {
-    mapRef.current = leaflet.map(`map`, {
-      center: {
-        lat: city.lat,
-        lng: city.lng
-      },
-      zoom: city.zoom
-    });
-
-    leaflet
-    .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-      .addTo(mapRef.current);
-
-    points.forEach((point) => {
-      const customIcon = leaflet.icon({
-        iconUrl: `./img/pin.svg`,
-        iconSize: [30, 30]
+    if (mapRef.current) {
+      mapRef.current = leaflet.map(`map`, {
+        center: {
+          lat: city.lat,
+          lng: city.lng
+        },
+        zoom: city.zoom
       });
 
-      leaflet.marker({
-        lat: point.location.lat,
-        lng: point.location.lng
-      },
-      {
-        icon: customIcon
-      })
-      .addTo(mapRef.current)
-      .bindPopup(point.location.title);
+      leaflet
+        .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })
+          .addTo(mapRef.current);
 
-      return () => {
-        mapRef.current.remove();
-      };
-    });
-  }, []);
+      points.forEach((point) => {
+        const customIcon = leaflet.icon({
+          iconUrl: `./img/pin.svg`,
+          iconSize: [30, 30]
+        });
+
+        leaflet.marker({
+          lat: point.location.lat,
+          lng: point.location.lng
+        },
+        {
+          icon: customIcon
+        })
+        .addTo(mapRef.current)
+          .bindPopup(point.location.title);
+
+        return () => {
+          mapRef.current.remove();
+        };
+      });
+    }
+  }, [points]);
 
   return (
     <div id="map" style={{width: `100%`}} ref={mapRef}></div>
